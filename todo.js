92442a1d19332cd2353281383e39f95f6e722027
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-11 13:29:48 -0700

    cleand up code to use IDs

--- todo.js
+++ todo.js
@@ -3,17 +3,19 @@
 
 
 		var $element = $(event.target);
-		var $form = $element.closest("form");
-		var $inputs = $form.find("input");
+		
 
-		var taskName=$inputs.first().val();
-		var dueDate=$inputs.last().val();
+		var taskName=$("#taskName").val();
+		var dueDate=$("#dueDate").val();
 
 		var $list = $('#todoTable');
+		
+		if(taskName && dueDate){
 		$list.append('<tr> <td>' + taskName + '</td><td>' + dueDate + '\
-			</td><td><input type="checkbox"></td></tr>');				
+			</td><td><input type="checkbox"></td></tr>'
+			);				
 
-
+		}
 		console.log('it worked!',$inputs);
 		return false;
 
