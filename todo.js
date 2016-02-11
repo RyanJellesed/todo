69997a5529ce48984c3d9a95b697cbcf42443c75
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-11 14:07:32 -0700

    more stuff

--- todo.js
+++ todo.js
@@ -1,25 +1,37 @@
-	function sayItWorked(event){
-		event.preventDefault();
+var taskOne = {name: "do some stuff", date: "02/15/2016"};
+var taskTwo = {name: "do some more stuff", date: "02/16/2016"};
+var $list = $('#todoTable');
+var todo = [taskOne, taskTwo];
 
-
-		var $element = $(event.target);
-		
-
-		var taskName=$("#taskName").val();
-		var dueDate=$("#dueDate").val();
-
-		var $list = $('#todoTable');
-		
-		if(taskName && dueDate){
-		$list.append('<tr> <td>' + taskName + '</td><td>' + dueDate + '\
+todo.forEach(function(todo){
+	$list.append('<tr> <td>' + todo.name + '</td><td>' + todo.date + '\
 			</td><td><input type="checkbox"></td></tr>'
-			);				
+			);
+})
 
-		}
-		console.log('it worked!',$inputs);
-		return false;
+
+  
+
+
+
+function createNewTodoFromForm(event){
+	event.preventDefault();
+
+	var taskName=$("#taskName").val();
+	var dueDate=$("#dueDate").val();
+
+	if(taskName && dueDate){
+	$list.append('<tr> <td>' + taskName + '</td><td>' + dueDate + '\
+		</td><td><input type="checkbox"></td></tr>'
+		);				
 
 	}
+	console.log('it worked!',$inputs);
+	return false;
 
-$('#submit-button').on('click', sayItWorked)
+}
+
+$('#submit-button').on('click', createNewTodoFromForm)
+
+
 
